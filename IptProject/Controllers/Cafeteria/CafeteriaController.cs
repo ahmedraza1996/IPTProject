db706e6c19b2c9df8f5af7d4eb2b6b5cb2ca254e
Ahmed Raza <ahmedrazamehmood@gmail.com>
2020-04-16 14:45:37 +0500

    Initial Commit- template added

---
+++ IptProject/Controllers/Cafeteria/CafeteriaController.cs
@@ -0,0 +1,49 @@
+﻿using IptProject.Models.Cafeteria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IptProject.Controllers
+{
+    public class CafeteriaController : Controller
+    {
+        // GET: Cafeteria
+        public ActionResult GetProduct()
+        {
+            List<FoodItem> lstFoodItems = new List<FoodItem>();
+            //FoodItem obj1 = new FoodItem(1, "Tikka", "avc", "Available", 200);
+            //FoodItem obj2 = new FoodItem(2, "Pizza", "avc", "Available", 100);
+            //lstFoodItems.Add(obj1);
+            //lstFoodItems.Add(obj2);
+
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("cafeteria/getproduct");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var readTask = result.Content.ReadAsAsync<FoodItem[]>();
+                    readTask.Wait();
+
+                    var fooditems = readTask.Result;
+
+                    foreach (var item in fooditems)
+                    {
+                        lstFoodItems.Add(item);
+                    }
+                }
+            }
+            
+            return View(lstFoodItems);
+        }
+    }
+}
---
+++ IptProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IptProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IptProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9848d5b2-1d81-4381-a9d4-1035f0d3a13b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
