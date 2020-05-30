8ca3f33f6c5e6bba95600df738fefc36bfe2c299
Salman Arshad <k163898@nu.du.pk>
2020-05-30 17:19:34 +0500

    Course item  display done

--- IptProject/Controllers/Cafeteria/CafeteriaController.cs
+++ IptProject/Controllers/Cafeteria/CafeteriaController.cs
@@ -1,8 +1,6 @@
 ﻿using IptProject.Models.Cafeteria;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Web;
@@ -24,7 +22,7 @@ namespace IptProject.Controllers
 
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("https://localhost:44380/api/");
+                client.BaseAddress = new Uri("http://localhost:44380/api/");
                 //HTTP GET
                 var responseTask = client.GetAsync("cafeteria/getproduct");
                 responseTask.Wait();
@@ -45,53 +43,7 @@ namespace IptProject.Controllers
                 }
             }
 
-            return View();
+            return View(lstFoodItems);
         }
-        public ActionResult fetchImage()
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("https://localhost:44380/api/");
-                //HTTP GET
-                var responseTask = client.GetAsync("testCafeteria/GetProductWithImage");
-                responseTask.Wait();
-
-                var result = responseTask.Result;
-                if (result.IsSuccessStatusCode)
-                {
-
-                    var readTask = result.Content.ReadAsAsync<Dictionary<string, object>>();
-                    readTask.Wait();
-
-                    var item = readTask.Result;
-                    var base64string = item["base64string"];
-                    var contents = Convert.FromBase64String(base64string.ToString());
-
-                    MemoryStream ms = new MemoryStream(contents);
-                    Image returnImage = Image.FromStream(ms);
-
-                    //foreach (var item in fooditems)
-                    //{
-                    //    lstFoodItems.Add(item);
-                    //}
-                    ViewBag.Image = returnImage;
-                }
-            }
-            return View();
-        }
-        public ActionResult GetImage()
-        {
-            List<FoodItem> lstFoodItems = new List<FoodItem>();
-            //FoodItem obj1 = new FoodItem(1, "Tikka", "avc", "Available", 200);
-            //FoodItem obj2 = new FoodItem(2, "Pizza", "avc", "Available", 100);
-            //lstFoodItems.Add(obj1);
-            //lstFoodItems.Add(obj2);
-
-
-          
-            return View();
-        }
-
     }
 }
-
