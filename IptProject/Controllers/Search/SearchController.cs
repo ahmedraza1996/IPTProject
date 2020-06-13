31312c0058ccd883206becc771a98e41614fd759
Muhammad Yasir Javed <k163614@nu.edu.pk>
2020-06-14 03:52:13 +0500

    search module views added
    search module

---
+++ IptProject/Controllers/Search/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IptProject.Controllers.Search
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+
+
+            return View();
+        }
+    }
+}
