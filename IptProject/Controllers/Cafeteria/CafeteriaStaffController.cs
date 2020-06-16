365f50a9c3ab5c7dda8e171e8bdeba4952d2c068
AwaisNizamani16 <awaisnizamani125@gmail.com>
2020-06-16 20:17:51 +0500

    Awais Nizamani Added Wallet

---
+++ IptProject/Controllers/Cafeteria/CafeteriaStaffController.cs
@@ -0,0 +1,28 @@
+﻿using IptProject.Models.Cafeteria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IptProject.Controllers.Cafeteria
+{
+    public class CafeteriaStaffController : Controller
+    {
+        // GET: CafeteriaStaff
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TopUpWallet()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TopUpWallet(Wallet obj)
+        {
+            return View();
+        }
+    }
+}
